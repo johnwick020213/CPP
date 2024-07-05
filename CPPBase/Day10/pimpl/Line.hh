73c13af2eb10ff502e67b877fb795fb0816a6ae4
johnwick020213 <ganbo020213@gmail.com>
2024-07-05 22:06:54 +0800

    pimpl工作模式

---
+++ CPPBase/Day10/pimpl/Line.hh
@@ -0,0 +1,20 @@
+#ifndef __Line_HH__
+#define __Line_HH__
+
+
+class Line{
+public:
+    Line(int x1, int y1, int x2, int y2);
+    ~Line();
+
+    //提供给客户使用的功能
+    void printLine() const;//打印Line对象的信息
+private:
+    class LineImpl;//类的前向声明
+    LineImpl * _pimpl;
+};
+
+
+
+#endif
+
