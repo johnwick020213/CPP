7e778b17c3256067f3a2dcbe221c213f7e8f5a06
johnwick020213 <ganbo020213@gmail.com>
2024-07-19 09:35:18 +0800

    增加智能指针

--- CPPBase/Day21/LRU.cc
+++ CPPBase/Day21/LRU.cc
@@ -1,13 +1,13 @@
 #include <iostream>
 #include<unordered_map>
-
+#include<memory>
 using namespace::std;
 
 //构建双向链表
 struct Node
 {
     int key,val;
-    Node* prev,*next;
+    shared_ptr<Node> prev,next;
     Node():key(0),val(0),prev(nullptr),next(nullptr){}
     Node(int _key,int _val):key(_key),val(_val),prev(nullptr),next(nullptr){}
 };
@@ -18,8 +18,8 @@ public:
     //构造函数，初始化双向链表
     LRUCache(int _capacity):capacity(_capacity),size(0)
     {
-          head=new Node();
-          tail=new Node();
+          head=make_shared<Node>();
+          tail=make_shared<Node>();
           head->next=tail;
           tail->prev=head;
     }
@@ -27,7 +27,7 @@ public:
     int get(int key)
     {
         if(!h.count(key))return -1;
-        Node*node=h[key];
+        auto node=h[key];
         removeNode(node);//删除节点
         addNodeToHead(node);//头部添加
         return node->val;
@@ -38,7 +38,7 @@ public:
         //哈希表中存在
         if(h.count(key))
         {
-            Node*node=h[key];
+            auto node=h[key];
             node->val=value;
             removeNode(node);
             addNodeToHead(node);
@@ -49,48 +49,50 @@ public:
             //容量已满，尾部删除
             if(size==capacity)
             {
-                Node *rm=tail->prev;
+                auto rm=tail->prev;
                 h.erase(rm->key);
                 removeNode(rm);
                 size--;
             }
-            Node*node=new Node(key,value);
+            auto node=make_shared< Node>(key,value);
             addNodeToHead(node);
             h[key]=node;
             size++;
         }
     }
 
-    void removeNode(Node *node)
+    void removeNode(shared_ptr<Node>node)
     {
         node->prev->next=node->next;
         node->next->prev=node->prev;
     }
 
-    void addNodeToHead(Node*node)
+    void addNodeToHead(shared_ptr<Node>node)
     {
         node->prev=head;
         node->next=head->next;
         head->next=node;
     }
 private:
-    Node*head,*tail;
-    unordered_map<int,Node*> h;
+    shared_ptr<Node>head,tail;
+    unordered_map<int,shared_ptr<Node>> h;
     int capacity,size;
 };
 
 int main()
 {
-    LRUCache lRUCache(2);
-    lRUCache.put(1, 1); // 缓存是 {1=1}
-    lRUCache.put(2, 2); // 缓存是 {1=1, 2=2}
-    lRUCache.get(1); // 返回 1
-    lRUCache.put(3, 3); // 该操作会使得关键字 2 作废，缓存是 {1=1, 3=3}
-    lRUCache.get(2); // 返回 -1 (未找到)
-    lRUCache.put(4, 4); // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}
-    lRUCache.get(1); // 返回 -1 (未找到)
-    lRUCache.get(3); // 返回 3
-    lRUCache.get(4); // 返回 4
+    {
+         LRUCache lRUCache(2);
+         lRUCache.put(1, 1); // 缓存是 {1=1}
+         lRUCache.put(2, 2); // 缓存是 {1=1, 2=2}
+         lRUCache.get(1); // 返回 1
+         lRUCache.put(3, 3); // 该操作会使得关键字 2 作废，缓存是 {1=1, 3=3}
+         lRUCache.get(2); // 返回 -1 (未找到)
+         lRUCache.put(4, 4); // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}
+         lRUCache.get(1); // 返回 -1 (未找到)
+         lRUCache.get(3); // 返回 3
+         lRUCache.get(4); // 返回 4
+    }
     return 0;
 }
 
